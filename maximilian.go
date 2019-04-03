a24f923f014ad9f727bf4e7db4a7c6dcf92f9247
kalvkusk <kalvis@rainforestapp.com>
2019-04-03 21:40:00 +0300

    bbb

--- maximilian.go
+++ maximilian.go
@@ -12,8 +12,6 @@ func maxim() {
 	if err == nil {
 		fmt.Println("pids...", fpid)
 		if len(fpid) > 0 {
-			 robotgo.ActivePID(fpid[0])
-			 hwnd := robotgo.GetHWND()
 			 robotgo.MaxWindow(fpid[0])
 
 		}
